f604bb836af85ddf666018cf5b8a84c1f51105d0
Papop Somnok <papop.s@kkumail.com>
2026-03-17 20:29:51 +0700

    add new user manual

--- code/frontend/nuxt.config.js
+++ code/frontend/nuxt.config.js
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: "https://organize-faq-fiber-successful.trycloudflare.com/api/",
+      apiBase: "https://karma-breath-sources-andale.trycloudflare.com/api/",
       // apiBase:"https://painamnae-backend.onrender.com/api/",
       googleMapsApiKey: process.env.NUXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""
     },
